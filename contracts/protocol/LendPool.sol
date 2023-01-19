81323f80e40d532738f77187a82719c44304b423
0xadrii <apajaresaguilera@gmail.com>
2023-01-19 18:44:20 +0100

    Credit BendDAO

--- contracts/protocol/LendPool.sol
+++ contracts/protocol/LendPool.sol
@@ -43,7 +43,7 @@ import {ContextUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/Cont
  * - To be covered by a proxy contract, owned by the LendPoolAddressesProvider of the specific market
  * - All admin functions are callable by the LendPoolConfigurator contract defined also in the
  *   LendPoolAddressesProvider
- * @author Unlockd
+ * @author Unlockd - Forked from BendDAO protocol (https://github.com/BendDAO/bend-lending-protocol)
  **/
 // !!! For Upgradable: DO NOT ADJUST Inheritance Order !!!
 contract LendPool is
